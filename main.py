1038f528c87f69d4e356f454df9438e67c7c77bc
Randy Arbolaez <randyarbolaez@Randys-MacBook-Pro.local>
2024-12-27 16:40:45 -0500

    error handling for input nums, making sure that user isn't inputting anything non-numeric

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 print("Welcome to Calculator")
 
-num_one = int(input("Input first number: "))
-num_two = int(input("Input second number: "))
-operator = input("Input an operator(+,-,/,x): ")
+num_one = input("Enter first number: ")
+num_two = input("Enter second number: ")
+operator = input("Enter an operator(+,-,/,x): ")
+
+while (not num_one.isnumeric()) or (not num_two.isnumeric()):
+    if(not num_one.isnumeric()):
+        num_one = input("only integers are allowed! Please enter an integer for the first number: ")
+    if(not num_two.isnumeric()):
+        num_two = input("only integers are allowed! Please enter an integer for the second number: ")
+
+num_one = int(num_one)
+num_two = int(num_two)
 
 match operator:
     case "+":
